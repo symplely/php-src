5af1ce3de9c674db867f24220e3765b541fd7e1f
Lawrence Stubbs <lstubbs@techno.express>
2023-05-01 07:08:10 -0400

    chore: update .gitignore

--- config.nice.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cscript /nologo /e:jscript configure.js  "--enable-cli" "--with-ffi" "--with-iconv" "--enable-phar" "--enable-filter" "--with-openssl" "--enable-sockets" "--enable-mbstring" "--with-libxml" "--enable-fileinfo" "--enable-xmlwriter" "--enable-tokenizer" "--disable-phpdbg" "--enable-embed" %*
